3e48c7f0963505504e9c2a876688eb3a10da0f00
ParthikB <rockon.parthik555@gmail.com>
2020-04-10 14:24:52 +0530

    added a find_file() function

--- static/index.js
+++ static/index.js
@@ -33,3 +33,38 @@ $('#fname').bind('change', function () {
 });
 
 
+
+
+//// A function that would return the contents of the JSON file located
+//// in the same dir as this script
+
+// function find_file(){
+//   const { readdirSync } = require('fs');
+//   const PATH_TO_YOUR_FOLDER = '.';
+  
+//   var file_found = false
+//   while (file_found==false) {  
+
+//     dir_files = readdirSync(PATH_TO_YOUR_FOLDER)
+
+//     for (var i=0; i<dir_files.length; i++) {
+//       fname = dir_files[i]
+//       if (fname=='info.json') {
+//         var file_found = true
+//         console.log('File Found')
+//         return require('./info');
+//       }
+//     console.log('File not found. Recursing...')
+//       }
+//   }
+// }
+
+
+////// data is the contents of the JSON file, which can be splitted as follows
+//// var data = find_file()
+
+//// var class_name = data['class'] 
+//// var prob_dist  = data['prob']
+
+//// console.log(class_name)
+//// console.log(prob_dist)
